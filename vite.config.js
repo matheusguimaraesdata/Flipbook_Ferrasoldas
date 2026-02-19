71e81a0d8a27eca4b498cae9c38669ac198f00c8
Matheus Guimarães <mfilipeglima@gmail.com>
2026-02-19 11:16:49 -0300

    correção: Alterando configuração vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  root: '.',            
-  base: './',           
+  plugins: [react()],          
   build: {
     outDir: 'dist',     
-    emptyOutDir: true,  
-  },
-  server: {
-    port: 5173,
-  },
+    emptyOutDir: true,
+  }
 })
